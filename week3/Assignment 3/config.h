f2020653913ccabd33daab852994269edae7f290
piyusha sharma <piyushas004@gmail.com>
2026-07-07 02:30:53 +0530

    Add files via upload

---
+++ week3/Assignment 3/config.h
@@ -0,0 +1,17 @@
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+
+#include <DHT.h>
+#include <DHT_U.h>
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char* WIFI_SSID = "OnePlus";
+const char* WIFI_PASSWORD = "12042006";
+
+const char* API_KEY ="70a00029068d63e402fadb62192b2dd5";
+
+const char* CITY = "jammu";
+
+#endif
